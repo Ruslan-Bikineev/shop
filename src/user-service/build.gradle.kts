4f80a7dfc8f4f600e839c8d93ef4236f65e0c0e7
Ruslan Bikineev <biken_r@mail.ru>
2026-07-11 14:42:38 +0300

    2: доработан build.gradle.kts

--- src/user-service/build.gradle.kts
+++ src/user-service/build.gradle.kts
@@ -1,21 +1,17 @@
 import com.google.protobuf.gradle.id
 
-val grpcVersion by extra("1.77.0")
-val protobufVersion by extra("4.33.0")
 val liquibaseVersion by extra("4.31.1")
-val springGrpcVersion by extra("1.0.0-SNAPSHOT")
 
-val javaVersion = "21"
-val pgvVersion = "1.2.1"
+val javaVersion = "25"
+val pgvVersion = "1.3.3"
 val mapstructVersion = "1.6.3"
 val jsonwebtokenVersion = "0.12.6"
-val nettyTransportVersion = "4.1.103.Final:linux-x86_64"
 
 plugins {
     java
-    id("org.springframework.boot") version "4.0.0"
+    id("org.springframework.boot") version "4.1.0"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.google.protobuf") version "0.9.5"
+    id("com.google.protobuf") version "0.9.6"
 }
 
 group = "org.springframework.grpc"
@@ -30,58 +26,37 @@ java {
 
 repositories {
     mavenCentral()
-    maven {
-        url = uri("https://repo.spring.io/milestone")
-    }
-    maven {
-        url = uri("https://repo.spring.io/snapshot")
-    }
-    maven {
-        url = uri("https://buf.build/gen/maven")
-    }
-}
-
-dependencyManagement {
-    imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:4.0.0")
-    }
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-grpc-server")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-json")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-    implementation("org.springframework.grpc:spring-grpc-spring-boot-starter:$springGrpcVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-liquibase")
-    implementation("org.projectlombok:lombok")
     implementation("org.liquibase:liquibase-core:$liquibaseVersion")
     implementation("org.mapstruct:mapstruct:$mapstructVersion")
     implementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
     implementation("build.buf.protoc-gen-validate:pgv-java-stub:$pgvVersion")
 
-    testImplementation("org.springframework.grpc:spring-grpc-test:$springGrpcVersion")
-    testImplementation("io.grpc:grpc-testing:$grpcVersion")
-    testImplementation("io.grpc:grpc-inprocess:$grpcVersion")
-    testImplementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    testImplementation("org.springframework.boot:spring-boot-starter-grpc-server-test")
 
-    runtimeOnly("io.netty:netty-transport-native-epoll:$nettyTransportVersion")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
 
-    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
+    compileOnly("org.projectlombok:lombok")
+
     annotationProcessor("org.projectlombok:lombok")
+    annotationProcessor("org.mapstruct:mapstruct-processor:$mapstructVersion")
 }
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:$protobufVersion"
+        artifact = "com.google.protobuf:protoc"
     }
     plugins {
-        id("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
-        }
         id("javapgv") {
             artifact = "build.buf.protoc-gen-validate:protoc-gen-validate:$pgvVersion"
         }
@@ -89,7 +64,6 @@ protobuf {
     generateProtoTasks {
         all().forEach {
             it.plugins {
-                id("grpc")
                 id("javapgv") {
                     option("lang=java")
                 }
@@ -100,16 +74,4 @@ protobuf {
 
 tasks.withType<Test> {
     useJUnitPlatform()
-}
-
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "com.google.protobuf") {
-            useVersion("$protobufVersion")
-            because("Align all protobuf dependencies to match protoc 4.x")
-        }
-        if (requested.group == "io.grpc") {
-            useVersion("$grpcVersion")
-        }
-    }
 }
